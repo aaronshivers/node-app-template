18e5b3795dd81cad83fb12cc39969fb378b8fed3
aaronshivers <aaron@moonmail.org>
2019-03-01 08:49:18 -0600

    added users.test.js

---
+++ tests/integration/users.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest')
+const { app } = require('../../app.js')
+
+describe('/', () => {
+
+  describe('GET /users', () => {
+    it('should respond 401 if user is not admin', () => {})
+    it('should respond 200', () => {})
+  })
+  describe('POST /users/new', () => {
+    it('should respond 400 if data is invalid', () => {})
+    it('should respond 400 if user is already in DB', () => {})
+    it('should save the new user', () => {})
+    it('should respond 200 and return the new user', () => {})
+  })
+  describe('GET /users/:id/view', () => {
+    it('should respond 401 if user is not admin or owner', () => {})
+    it('should respond 404 if ObjectId is invalid', () => {})
+    it('should respond 404 if ObjectId is not in DB', () => {})
+    it('should respond 200', () => {})
+  })
+  describe('PATCH /users/:id/edit', () => {
+    it('should respond 401 if user is not admin or owner', () => {})
+    it('should respond 404 if ObjectId is invalid', () => {})
+    it('should respond 404 if ObjectId is not in DB', () => {})
+    it('should save the updated user', () => {})
+    it('should respond 200 and return the updated user', () => {})
+  })
+  describe('DELETE /users/:id', () => {
+    it('should respond 401 if user is not admin or owner', () => {})
+    it('should respond 404 if ObjectId is invalid', () => {})
+    it('should respond 404 if ObjectId is not in DB', () => {})
+    it('should delete the specified user', () => {})
+    it('should respond 200 and return the deleted user', () => {})
+  })
+  describe('GET /users/login', () => {
+    it('should respond 400 if data is invalid', () => {})
+    it('should respond 404 if user is not in DB', () => {})
+    it('should create an authentication token', () => {})
+    it('should respond 200 and return the authentication token', () => {})
+  })
+})
