c4433da9b90bdbea0579ed5daf2f17d5ca243ba5
aaronshivers <aaron@moonmail.org>
2019-03-01 10:21:44 -0600

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../models/users')
+
+module.exports = async (req, res, next) => {
+  const secret = process.env.JWT_SECRET
+  const token = req.header('x-auth-token')
+  if (!token) return res.status(401).send('Access Denied! No token provided.')
+  
+  try {
+    req.user = await jwt.verify(token, secret)
+    next()
+  } catch (error) {
+    res.status(400).send('Invalid Token')
+  }
+}
