45e0dad6e36300ac13096aaeab3c9634d9792918
aaronshivers <aaron@moonmail.org>
2019-03-01 20:35:26 -0600

    got auth.test.js working

---
+++ tests/unit/middleware/auth.test.js
@@ -0,0 +1,20 @@
+const User = require('../../../models/users')
+const auth = require('../../../middleware/auth')
+const { ObjectId } = require('mongodb')
+
+describe('auth middleware', () => {
+  it('should populate req.user with the valid JWT', () => {
+    const user = { _id: new ObjectId(), isAdmin: true }
+    const token = new User(user).createAuthToken()
+    const req = {
+      header: jest.fn().mockReturnValue(token)
+    }
+    const res = {}
+    const next = jest.fn()
+
+    auth(req, res, next)
+
+    expect(req.user).toHaveProperty('_id', user._id.toString())
+    expect(req.user).toHaveProperty('isAdmin', true)
+  })
+})
